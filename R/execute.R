0c818219857456833507388d87f3f37da7171db8
Alex Pavlakis <apavlaki@apavlaki-mn1.linkedin.biz>
2020-08-27 11:07:11 -0700

    update

--- R/execute.R
+++ R/execute.R
@@ -19,14 +19,6 @@ system("open docs/index.html")
 
 
 
-dates <- seq.Date(as.Date("2020-07-16"), Sys.Date(), by = 'day')
-
-for(i in 1:length(dates)) {
-  exec_date <- dates[i]
-  print(exec_date)
-  source("R/fit_yapa.R")
-}
-
 
 
 
@@ -42,25 +34,13 @@ for(i in 1:length(dates)) {
   print(Sys.time() - start)
 }
 
-exec_date <- Sys.Date()
-source("R/fit_house.R")
-
-colMeans(efgb$theta_gb)
 
 
-f1 <- data_frame(
-  datepartition = c('2020-06-21', '2020-06-21'),
-  contract_id = c(12345, 12345),
-  member_id = c(6789, 6789),
-  funnel_stage = c('aware', 'engaged')
-)
-
-f1 %>% knitr::kable()
-
-f1 %>%
-  group_by(datepartition, contract_id, member_id) %>%
-  summarise(aware_actions = sum(funnel_stage == 'aware'),
-            engaged_actions = sum(funnel_stage == 'engaged'),
-            considering_actions = sum(funnel_stage == 'considering')) %>%
-  ungroup() %>%
-  knitr::kable()
+# Tipping point
+p_biden %>%
+  left_join(
+    prior_results %>%
+      select(state, ev)
+  ) %>%
+  arrange(-p_biden) %>%
+  mutate(total_ev = cumsum(ev))
