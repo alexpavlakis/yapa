b465fc70271d273b93fed70444779ad1942bd96f
Alex Pavlakis <apavlaki@apavlaki-mn1.linkedin.biz>
2020-10-01 12:57:34 -0700

    update

--- R/execute.R
+++ R/execute.R
@@ -4,8 +4,8 @@ exec_date <- Sys.Date()
 start <- Sys.time()
 
 source("R/fit_yapa.R")
-source("R/fit_house.R")
-source("R/fit_senate.R")
+#source("R/fit_house.R")
+#source("R/fit_senate.R")
 
 # Render site
 setwd("docs")
@@ -22,14 +22,14 @@ dim(polls)
 fit <- yapa(y = select(polls, `Biden (D)`, `Trump (R)`), n = polls$Sample, dates = polls$end_date)
 plot(fit)
 
-dates <- seq.Date(as.Date("2020-09-19"), Sys.Date(), by = 'day')
+dates <- seq.Date(as.Date("2020-09-24"), Sys.Date(), by = 'day')
 
 for(i in 1:length(dates)) {
   exec_date <- dates[i]
   start <- Sys.time()
   print(exec_date)
-  source("R/fit_house.R")
-  source("R/fit_senate.R")
+  #source("R/fit_house.R")
+  #source("R/fit_senate.R")
   source("R/fit_yapa.R")
   print(Sys.time() - start)
 }
